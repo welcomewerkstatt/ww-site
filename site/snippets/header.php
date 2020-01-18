bb2c2da0ff1461bff4fc48bdb2fd6dff81097344
Moritz Stueckler <moritz.stueckler@gmail.com>
2020-01-18 15:54:41 +0100

    feat: use global images if no page images set

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -14,16 +14,22 @@
     <div class="logo-row full-column blue"></div>
     <header class="logo-row main-column"></header>
     <div class="slider-row full-column blue flex-container space-between">
-      
+
       <?php if ($page->files()->isNotEmpty()) : ?>
         <?php foreach ($page->files()->sortBy('sort') as $image) : ?>
           <img src="<?= $image->mediaUrl() ?>" />
         <?php endforeach ?>
       <?php else : ?>
-        <img src="https://picsum.photos/1280/720?random=1">
-        <img src="https://picsum.photos/1280/853?random=2">
-        <img src="https://picsum.photos/600/600?random=3">
-        <img src="https://picsum.photos/1280/720?random=4">
+        <?php if ($site->files()->isNotEmpty()) : ?>
+          <?php foreach ($site->files()->sortBy('sort') as $image) : ?>
+            <img src="<?= $image->mediaUrl() ?>" />
+          <?php endforeach ?>
+        <?php else : ?>
+          <img src="https://picsum.photos/1280/720?random=1">
+          <img src="https://picsum.photos/1280/853?random=2">
+          <img src="https://picsum.photos/600/600?random=3">
+          <img src="https://picsum.photos/1280/720?random=4">
+        <?php endif ?>
       <?php endif ?>
 
     </div>
