00807c1aab3befeca8e3ae081a8576e0aef47572
Moritz Stueckler <moritz.stueckler@gmail.com>
2020-01-25 19:03:48 +0100

    feat: download button

---
+++ site/plugins/werkstatt/index.php
@@ -0,0 +1,31 @@
+<?php
+
+// Usage: (button: link/target caption: My button)
+
+Kirby::plugin('welcome-werkstatt/werkstatt', [
+  'tags' => [
+    'button' => [
+      'attr' => [
+        'caption',
+        'icon'
+      ],
+      'html' => function ($tag) {
+        $target = Url::to($tag->value);
+        $icon = empty($tag->icon) ? '' : '<img style="display: inline-block; width: auto" width="16px" height="16px" src="/assets/img/download.svg" /> ';
+
+        return '<a class="button" href="' . $target . '"/>' . $icon .'<span style="display: inline-block;">'. $tag->caption . '</span></a>';
+      }
+    ],
+    'external' => [
+      'html' => function ($tag) {
+        $external = file_get_contents($tag->value);
+        return $external;
+      }
+    ],
+    'embed' => [
+      'html' => function ($tag) {
+        return '<div class="embed">' . $tag->value . '</div>';
+      }
+    ]
+  ]
+]);
