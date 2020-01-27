d3830df62f6a653f1a46265c3366b5294ec74aef
Moritz Stueckler <moritz.stueckler@gmail.com>
2020-01-27 08:20:34 +0100

    fix: add redirect for old route

--- site/plugins/werkstatt/index.php
+++ site/plugins/werkstatt/index.php
@@ -2,30 +2,42 @@
 
 // Usage: (button: link/target caption: My button)
 
-Kirby::plugin('welcome-werkstatt/werkstatt', [
-  'tags' => [
-    'button' => [
-      'attr' => [
-        'caption',
-        'icon'
-      ],
-      'html' => function ($tag) {
-        $target = Url::to($tag->value);
-        $icon = empty($tag->icon) ? '' : '<img style="display: inline-block; width: auto" width="16px" height="16px" src="/assets/img/download.svg" /> ';
+Kirby::plugin(
+  'welcome-werkstatt/werkstatt',
+  [
+    'tags' => [
+      'button' => [
+        'attr' => [
+          'caption',
+          'icon'
+        ],
+        'html' => function ($tag) {
+          $target = Url::to($tag->value);
+          $icon = empty($tag->icon) ? '' : '<img style="display: inline-block; width: auto" width="16px" height="16px" src="/assets/img/download.svg" /> ';
 
-        return '<a class="button" href="' . $target . '"/>' . $icon .'<span style="display: inline-block;">'. $tag->caption . '</span></a>';
-      }
+          return '<a class="button" href="' . $target . '"/>' . $icon . '<span style="display: inline-block;">' . $tag->caption . '</span></a>';
+        }
+      ],
+      'external' => [
+        'html' => function ($tag) {
+          $external = file_get_contents($tag->value);
+          return $external;
+        }
+      ],
+      'embed' => [
+        'html' => function ($tag) {
+          return '<div class="embed-container">' . $tag->value . '</div>';
+        }
+      ],
     ],
-    'external' => [
-      'html' => function ($tag) {
-        $external = file_get_contents($tag->value);
-        return $external;
-      }
-    ],
-    'embed' => [
-      'html' => function ($tag) {
-        return '<div class="embed-container">' . $tag->value . '</div>';
-      }
+    // Redirects from old website
+    'routes' => [
+      [
+        'pattern' => 'raeume',
+        'action' => function () {
+          go('/werkstatt/holzarbeiten', 301);
+        }
+      ]
     ]
   ]
-]);
+);
