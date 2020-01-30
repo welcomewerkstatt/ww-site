556e618f0f9ac3f155c74b675407552c92899eaf
Moritz Stueckler <moritz.stueckler@gmail.com>
2020-01-30 21:16:22 +0100

    fix: defer vimeo script

--- site/snippets/blocks/vimeo.php
+++ site/snippets/blocks/vimeo.php
@@ -2,4 +2,4 @@
 <div style="padding:56.25% 0 0 0;position:relative;">
   <iframe src="<?= $data->url() ?>" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
 </div>
-<script src="https://player.vimeo.com/api/player.js"></script>
+<script defer src="https://player.vimeo.com/api/player.js"></script>
