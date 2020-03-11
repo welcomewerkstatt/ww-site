6efeb045d0411d9e773b1973e2725755f584a065
Moritz Stueckler <moritz.stueckler@gmail.com>
2020-03-11 07:35:11 +0100

    feat: finish mapbox plugin

--- site/plugins/mapbox-embed/index.php
+++ site/plugins/mapbox-embed/index.php
@@ -1,13 +1,10 @@
 <?php
 
 Kirby::plugin('welcome-werkstatt/mapbox-embed', [
-  'snippets' => [
-    'includes' => __DIR__ . '/snippets/includes.php'
-  ],
   'tags' => [
     'mapbox' => [
       'html' => function ($tag) {
-        return '<div id="map"></div>' . css('media/plugins/welcome-werkstatt/mapbox-embed/css/map.css') . js('/media/plugins/welcome-werkstatt/mapbox-embed/js/mapbox-gl.js', ['defer' => true]) . js('/media/plugins/welcome-werkstatt/mapbox-embed/js/map.js', ['defer' => true]);
+        return '<div id="map"></div>' . css('media/plugins/welcome-werkstatt/mapbox-embed/css/map.css') . js('/media/plugins/welcome-werkstatt/mapbox-embed/js/mapbox-gl.js') . js('/media/plugins/welcome-werkstatt/mapbox-embed/js/map.js');
       }
     ]
   ]
--- site/plugins/mapbox-embed/snippets/includes.php
+++
@@ -1,3 +0,0 @@
-<?= css('assets/css/map.css') ?>
-<?= js('assets/js/mapbox-gl.js', ['defer' => true]) ?>
-<?= js('assets/js/map.js', ['defer' => true]) ?>
