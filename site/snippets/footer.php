14ed14742001ff8acbc78a50cdd1e3235a5b64c3
Moritz Stückler <stueckler@ubilabs.net>
2020-12-21 20:33:55 +0100

    fix: add conditional rendering

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -11,21 +11,27 @@
       <?php endforeach ?>
     </ul>
     <div class="social-media-icons">
-      <div class="social-media-icon">
-        <a href="<?= $site->instagram()->html() ?>">
-          <?= svg("assets/img/instagram_icon.svg") ?>
-        </a>
-      </div>
-      <div class="social-media-icon">
-        <a href="<?= $site->facebook()->html() ?>">
-          <?= svg("assets/img/facebook_icon.svg") ?>
-        </a>
-      </div>
-      <div class="social-media-icon">
-        <a href="<?= $site->twitter()->html() ?>">
-          <?= svg("assets/img/twitter_icon.svg") ?>
-        </a>
-      </div>
+      <?php if ($site->instagram()->isNotEmpty()) : ?>
+        <div class="social-media-icon">
+          <a href="<?= $site->instagram()->html() ?>">
+            <?= svg("assets/img/instagram_icon.svg") ?>
+          </a>
+        </div>
+      <?php endif ?>
+      <?php if ($site->facebook()->isNotEmpty()) : ?>
+        <div class="social-media-icon">
+          <a href="<?= $site->facebook()->html() ?>">
+            <?= svg("assets/img/facebook_icon.svg") ?>
+          </a>
+        </div>
+      <?php endif ?>
+      <?php if ($site->twitter()->isNotEmpty()) : ?>
+        <div class="social-media-icon">
+          <a href="<?= $site->twitter()->html() ?>">
+            <?= svg("assets/img/twitter_icon.svg") ?>
+          </a>
+        </div>
+      <?php endif ?>
     </div>
   </nav>
 
@@ -33,4 +39,5 @@
 </main>
 </body>
 <?php echo snippet('matomo') ?>
+
 </html>
