cd4c9e6f92999a01009a9130b7778a809d0a7c66
Moritz Stückler <stueckler@ubilabs.net>
2021-02-06 16:40:12 +0100

    feat: add layouts template and next-events block

--- site/snippets/content-blocks.php
+++ site/snippets/content-blocks.php
@@ -1,10 +1,19 @@
 <div class="content-row full-column grey"></div>
 <div class="content-row main-column flex-column">
+  <?php if ($page->headline()->isNotEmpty()) : ?>
+    <h1><?= $page->headline()->html() ?></h1>
+  <?php else : ?>
+    <h1><?= $page->title()->html() ?></h1>
+  <?php endif ?>
   <?php foreach ($page->text()->toLayouts() as $layout) : ?>
     <section class="content-layout" id="<?= $layout->id() ?>">
       <?php foreach ($layout->columns() as $column) : ?>
         <div class="blocks" style="flex: <?= $column->span() ?>">
-          <?= $column->blocks() ?>
+          <?php foreach ($column->blocks() as $block) : ?>
+            <div class="block">
+              <?= $block ?>
+            </div>
+          <?php endforeach ?>
         </div>
       <?php endforeach ?>
     </section>
