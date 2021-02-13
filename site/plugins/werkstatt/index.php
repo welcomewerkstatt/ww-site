fc7d47681191626146e54056f8dbecdbff9e92a6
Moritz Stückler <stueckler@ubilabs.net>
2021-02-13 22:46:25 +0100

    chore: remove old plugin code (moved to blocks)

--- site/plugins/werkstatt/index.php
+++ site/plugins/werkstatt/index.php
@@ -6,28 +6,6 @@ Kirby::plugin(
   'welcome-werkstatt/werkstatt',
   [
     'tags' => [
-      'paypal' => [
-        'attr' => [
-          'caption',
-        ],
-        'html' => function ($tag) {
-          $icon = '<img style="display:inline-block;vertical-align:middle;width:26px;height:26px" src="/assets/img/paypal_logo.svg" /> ';
-
-          return '<form action="https://www.paypal.com/donate" method="post" target="_top"><input type="hidden" name="hosted_button_id" value="W4JU2A668DTVW" /><button type="submit" class="button">' . $icon . '<span style="display:inline-block;vertical-align:middle;">' . $tag->caption . '</span></button><img alt="" border="0" src="https://www.paypal.com/de_DE/i/scr/pixel.gif" width="1" height="1" /></form>';
-        }
-      ],
-      'button' => [
-        'attr' => [
-          'caption',
-          'icon'
-        ],
-        'html' => function ($tag) {
-          $target = Url::to($tag->value);
-          $icon = empty($tag->icon) ? '' : '<img style="display: inline-block; width: auto" width="16px" height="16px" src="/assets/img/download.svg" /> ';
-
-          return '<a class="button" href="' . $target . '"/>' . $icon . '<span style="display: inline-block;">' . $tag->caption . '</span></a>';
-        }
-      ],
       'external' => [
         'html' => function ($tag) {
           return file_get_contents($tag->value);
@@ -71,10 +49,5 @@ Kirby::plugin(
         }
       ]
     ],
-    'options' => [
-      'cache' => true,
-      'calendarUrl' => 'https://cloud.welcome-werkstatt.de/remote.php/dav/public-calendars/xWWZXzBkDtzWgTPA?export',
-      'eventsToDisplay' => 3
-    ]
   ]
 );
