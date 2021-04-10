799cb38ff55956d9ad832c410c5ac5b0c255cc48
Moritz Stueckler <moritz@elbstack.com>
2021-04-10 17:47:12 +0200

    fix: styling for internal pages

--- site/snippets/internal/content.php
+++ site/snippets/internal/content.php
@@ -1,7 +1,9 @@
 <main class="full-height">
   <?php snippet('internal/menu') ?>
-  <section class="content markdown-body">
-    <h1><?= $page->title() ?></h1>
-    <?= $page->text()->kt() ?>
+  <div class="content">
+    <section class="markdown-body">
+      <h1><?= $page->title() ?></h1>
+      <?= $page->text()->kt() ?>
+  </div>
   </section>
 </main>
