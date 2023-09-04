3e4bdd15a5eb1bf9ab5cd341a5e24eac2d4dc312
Moritz Stückler <moritz@bitbetter.de>
2023-09-04 15:44:18 +0200

    fix: shortlink internal css

--- site/snippets/internal/content.php
+++ site/snippets/internal/content.php
@@ -1,7 +1,6 @@
 <main class="h-full">
-  <h1><?= $hasMenu ?></h1>
   <?php if ($hasMenu ?? false) snippet('internal/menu') ?>
-  <div class="content h-0-mobile">
+  <div class="<?= $hasMenu ? 'content' : 'content-no-menu'?> h-0-mobile">
     <?php snippet('internal/floating-button') ?>
     <section class="markdown-body max-w-980">
       <h1><?= $page->title() ?></h1>
